dd644b75a3648efcc35d2bc926059646e26d9039
Emmanuel Arhin <125448059+Tsikoviski@users.noreply.github.com>
2024-05-11 07:27:28 -0700

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,25 @@
-from flask_login import LoginManager, UserMixin
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from .models import User
 
+db = SQLAlchemy()
 login_manager = LoginManager()
 
-class User(UserMixin):
-    def __init__(self, user_id):
-        self.id = user_id
+def create_app():
+    app = Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///yourdatabase.db'
+    app.config['SECRET_KEY'] = 'your-secret-key'
+
+    db.init_app(app)
+    login_manager.init_app(app)
+    login_manager.login_view = 'auth.login'
+
+    from .routes import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+    return app
 
 @login_manager.user_loader
 def load_user(user_id):
-    # Load user from your database or other storage
-    return User(user_id)
+    return User.query.get(int(user_id))
